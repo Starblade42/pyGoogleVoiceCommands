ba67edc8d3b4175723ca60dd7c56e70db6a5e0e8
Jonathan Huff <zarcos@gmail.com>
2013-12-12 12:58:58 -0700

    I updated the file to include a bunch of comments to clarify the main function of the most important function: verifyReceivedPin()

--- detectRemoteCommand.py
+++ detectRemoteCommand.py
@@ -14,13 +14,11 @@ import datetime
 import random
 import subprocess
 
-debug = True
-
 smsCommands = {'on':'''Power on''', 'off':'''Shutdown''', 'off_force':'''Force shutdown'''}
 adminNumber = '8018306325'
 executibles = {'on':'''on_power.py''', 'off':'''on_power.py''', 'off_force':'''off_power_force.py'''}
 confirmedText = "I will now execute command "
-executibleLocation = "/home/jkh777/pyGoogleVoiceCommands/"
+executibleLocation = "/home/jkh777/"
 
 
 
@@ -42,36 +40,37 @@ def sendVerifyText(msg):
 	input = msg['text']
 	key = getSmsCommandKey(input)
 	pin = str(random.randint(0,99999)).zfill(5)
-	verifyText = "Do you want to \n --> " + input + " <-- \nyour computer? Reply with \n--> " + pin + " <--\nwithin 2 minutes. Anything else to abort"
+	verifyText = "Do you want to \n --> " + input + " <-- \nyour computer? Reply with \n--> " + pin + " <--\nwithin 5 minutes."
 	sendSMS(adminNumber,verifyText)
 	return pin
 
 def verifyReceivedPin(waitTime,pin,msg):
+	#Login so we can see if we got what we wanted
 	voice.login()
 	voice.sms()
+	#Extract the SMS so we can look at it in a dictionary
 	msgs = extractsms(voice.sms.html)
+	#find the last message we received (not perfect. Based on order on the page. Multiple conversations messes this up. Thanks Google Voice!)
 	myLastSms = findLastMsgISent(msgs)
-	if myLastSms == -1:
-		myLastSms == 0
+	#retrive the command being sent and find the proper key for our dictionary, so we can execute the right command in our command dictionary
 	commandKey = getSmsCommandKey(msg['text'])
+	#Set up the loop we will use to poll for the text. Eventually I'll make it so this can be a script that runs in response to a script, but polling will do
 	timeElapsed = 0
 	while timeElapsed < waitTime:
+		#log in to get all the most recent SMS messages.
 		voice.login()
 		voice.sms()
 		msgs = extractsms(voice.sms.html)
-
+		# Find the key and the number of the text message to make sure it matches the PIN we want.
+		#If it's not a text that is newer than the one we just sent, then it's not valid.
+		#This is important, because we might otherwise find the PIN we sent out and think we got a reply, even though we didn't!
+		#Self fulfilling prophecies are not allowed
 		for i,key in enumerate(msgs):
-			if isConfirmPin(msgs[i],pin) and (i > myLastSms):
+			if isConfirmPin(msgs[i],pin) & (i > myLastSms):
 				executeCommand(commandKey)
-				#deleteReadSMS()
+				deleteReadSMS()
 				return
-			elif not(isConfirmPin(msgs[i],pin)) and (i > myLastSms):
-				print "Command aborted."
-				print myLastSms , i
-				#sendSMS(adminNumber, "Command aborted")
-				#deleteReadSMS()
-				return
-
+		#Rather than poll constantly, we'll wait at least 30 seconds. Don't want Google to think we're trying to hack them!
 		sleep(30)
 		timeElapsed += 30
 
@@ -79,9 +78,13 @@ def verifyReceivedPin(waitTime,pin,msg):
 
 def executeCommand(key):
 	try:
+		#Use the subprocess library to call the executible we'll be running in order to complete the SMS command we received.
 		subprocess.check_call(executibleLocation + executibles[key])
+		#Tell our user that we got their command and just finished doing it for them!
 		sendSMS(adminNumber,confirmedText+executibles[key])
 		print datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p") + "I will now execute command " + executibles[key] + ".\n"
+		#We're finished! Lets clean up our inbox so things will work consistently next time!
+		deleteReadSMS()
 	except subprocess.CalledProcessError:
 		sendSMS(adminNumber, "Power operation >" + executibles[key] + "< failed.")
 		print datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p") + "Power operation>" + executibles[key] + "< failed due to CalledProcessError.\n"
@@ -90,11 +93,13 @@ def executeCommand(key):
 		print datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p") + "Power operation>" + executibles[key] + "< failed due to OS error.\n"
 
 def deleteReadSMS():
+	#Delete the read SMS messages. Just what it says on the tin.
 	for message in voice.sms().messages:
 		if message.isRead:
 			message.delete()
 
 def sendSMS(number,message):
+	#send a message to the NUMBER specified with the MESSAGE specified
 	voice.send_sms(number, message)
 
 def getSmsCommandKey(text):
@@ -147,15 +152,11 @@ voice = Voice()
 voice.login()
 voice.sms()
 msgs = extractsms(voice.sms.html)
-msgsLength = len(msgs)
-if debug:
-	print "msgsLength = " + str(msgsLength)
-if msgsLength >0:
-	msg = msgs[msgsLength-1]
-	key = ""
-	if isSMScommand(msg):
-		key = getSmsCommandKey(msg)
-		pin = sendVerifyText(msg)
-		verifyReceivedPin(2*60,pin,msg)
+msg = msgs[len(msgs)-1]
+key = ""
+if isSMScommand(msg):
+	key = getSmsCommandKey(msg)
+	pin = sendVerifyText(msg)
+	verifyReceivedPin(2*60,pin,msg)
 
 
